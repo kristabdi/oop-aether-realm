7b5d51df0fc195f227953a6fe5aceb9f45c4708c
Zeus-s <kristabdixx@gmail.com>
2022-04-26 20:06:49 +0700

    add reader csv

--- src/main/java/com/aetherwars/model/MorphSpell.java
+++ src/main/java/com/aetherwars/model/MorphSpell.java
@@ -1,3 +1,12 @@
 public class MorphSpell extends SpellCard {
-    private CharacterCard morphTarget;
+    private String morphTarget;
+
+    MorphSpell(String name, String type, String description, String imagepath, String targetName, int mana) {
+        super(name, type, description, mana, 0, 0, imagepath);
+        this.morphTarget = targetName;
+    }
+
+    public String getMorphTarget() {
+        return morphTarget;
+    }
 }
